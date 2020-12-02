6f19a419dff9e2b2b7c951c8037358ec145477c0
Oren Geva <o4oren@gmail.com>
2020-12-02 21:10:34 +0200

    All performance improvements, sans changes to storage

--- app/redux/actions/vatsimLiveDataActions.js
+++ app/redux/actions/vatsimLiveDataActions.js
@@ -1,3 +1,6 @@
+import getAircraftIcon from '../../util/aircraftIconResolver';
+import {PILOT, TWR_ATIS} from '../../util/consts';
+
 export const DATA_UPDATED = 'DATA_UPDATED';
 export const UPDATE_DATA = 'UPDATE_DATA';
 export const DATA_FETCH_ERROR = 'DATA_FETCH_ERROR';
@@ -15,9 +18,21 @@ const updateData = async (dispatch, getState) => {
             'https://data.vatsim.net/vatsim-data.json'
         );
         let json = await response.json();
-        // Uncomment to debug FSS when they are not available
+        const modifiedClients = json.clients.forEach(client => {
+            if(client.clienttype == PILOT) {
+                let icon = getAircraftIcon(client.planned_aircraft);
+                client.icon = icon;
+            } else if(client.facilitytype == TWR_ATIS && client.callsign.split('_').pop() == 'TWR') {
+                client.icon = require('../../../assets/tower-96.png');
+            }
+        });
+
+        //Uncomment to debug FSS when they are not available
         // json.clients.push(
         //     {callsign: 'EURS_FSS', clienttype: 'ATC', facilitytype: 1}
+        // );
+        // json.clients.push(
+        //     {callsign: 'CZEG_FSS', clienttype: 'ATC', facilitytype: 1}
         // );
         dispatch(dataUpdated(json));
     } catch (error) {
