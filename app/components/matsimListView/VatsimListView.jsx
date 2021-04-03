73244c85dc612026edfef83114931807cc0790e0
Oren Geva <o4oren@gmail.com>
2021-04-03 03:13:08 +0300

    Flatlist for list of pilots

--- app/components/matsimListView/VatsimListView.jsx
+++ app/components/matsimListView/VatsimListView.jsx
@@ -1,41 +1,45 @@
 import React from 'react';
-import {View, ScrollView, Text, StyleSheet, Dimensions} from 'react-native';
+import {StyleSheet, FlatList, SafeAreaView} from 'react-native';
 import {useSelector} from 'react-redux';
-import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import { Avatar, Card, Paragraph } from 'react-native-paper';
 
 export default function VatsimListView() {
     const clients = useSelector(state => state.vatsimLiveData.clients);
 
-    return <View>
-        <ScrollView>
-            {
-                clients.pilots.map(
-                    (client, index) => <Card key={index} style={styles.card}>
-                        <Card.Title
-                            title = {client.callsign}
-                            subtitle = {client.realname}
-                            left = {() => <Avatar.Image source={client.image} size={32} style={styles.avatar} />}
-                        />
-                        <Card.Content>
-                            <Paragraph>
-                                Altitude: {client.altitude}{'\n'}
-                                Ground speed: {client.groundspeed} kts{'\n'}
-                                Aircraft: {client.planned_aircraft}{'\n'}
-                                {client.planned_depairport} -> {client.planned_destairport}{'\n'}
-                            </Paragraph>
-                        </Card.Content>
-                    </Card>
-                )
-            }
-        </ScrollView>
-    </View>;
+    const Item = (client)=>(<Card style={styles.card}>
+        <Card.Title
+            title = {client.item.callsign}
+            subtitle = {client.item.realname}
+            left = {() => <Avatar.Image source={client.item.image} size={32} style={styles.avatar} />}
+        />
+        <Card.Content>
+            <Paragraph>
+                    Altitude: {client.item.altitude}{'\n'}
+                    Ground speed: {client.item.groundspeed} kts{'\n'}
+                    Aircraft: {client.item.planned_aircraft}{'\n'}
+                {client.item.planned_depairport} -&gt {client.item.planned_destairport}{'\n'}
+            </Paragraph>
+        </Card.Content>
+    </Card>
+    );
+
+    return <SafeAreaView style={styles.container}>
+        <FlatList
+            data = {clients.pilots}
+            renderItem={Item}
+            keyExtractor = {client => client.cid}
+        />
+    </SafeAreaView>;
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
     avatar: {
         backgroundColor: 'white',
     },
     card: {
-        margin: 10
+        margin: 10,
     },
 });
