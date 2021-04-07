9f8f066f2321dcc1cc730af8eeb2b2b5cbbceabd
Oren Geva <o4oren@gmail.com>
2021-04-08 01:01:20 +0300

    apps in airports

--- app/common/distance.js
+++ app/common/distance.js
@@ -1,18 +1,15 @@
+function deg2rad(deg) {
+    return deg * (Math.PI / 180);
+}
 
 export const getDistanceFromLatLonInNm = (point1, point2) => {
-    var R = 3440.1; // Radius of the earth in km
-    var dLat = deg2rad(point2.lat-point1.lat);  // deg2rad below
-    var dLon = deg2rad(point2.lon-point1.lon);
-    var a =
-        Math.sin(dLat/2) * Math.sin(dLat/2) +
-        Math.cos(deg2rad(point1.lat)) * Math.cos(deg2rad(point2.lat)) *
-        Math.sin(dLon/2) * Math.sin(dLon/2)
-    ;
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    var d = R * c; // Distance in km
+    const R = 3440.1; // Radius of the earth in km
+    const dLat = deg2rad(point2.lat - point1.lat); // deg2rad below
+    const dLon = deg2rad(point2.lon - point1.lon);
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+        + Math.cos(deg2rad(point1.lat)) * Math.cos(deg2rad(point2.lat))
+        * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c; // Distance in km
     return Math.trunc(d);
 };
-
-function deg2rad(deg) {
-    return deg * (Math.PI/180);
-}
