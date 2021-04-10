6a6ab3eb09a8dc4de3894b6169622624799a1b12
Oren Geva <o4oren@gmail.com>
2021-04-11 02:45:27 +0300

    Appbar and menu

---
+++ app/components/AppBar/AppBar.jsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import {Appbar, Divider, Menu} from 'react-native-paper';
+import {StyleSheet, View} from 'react-native';
+import {useState} from 'react';
+
+const AppBar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const openMenu = () => setShowMenu(true);
+    const closeMenu = () => setShowMenu(false);
+
+    return (
+        <Appbar.Header style={styles.appbar} dark={true}>
+            <Appbar.Content title="VatView" dark={true}/>
+            <Menu
+                visible={showMenu}
+                onDismiss={closeMenu}
+                anchor={
+                    <Appbar.Action icon="dots-vertical" color={'white'} onPress={openMenu} />
+                }
+            >
+                <Divider />
+                <Menu.Item onPress={() => {}} title="About" />
+            </Menu>
+        </Appbar.Header>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    appbar: {
+        alignSelf: 'flex-start'
+    }
+});
+
+export default AppBar;
