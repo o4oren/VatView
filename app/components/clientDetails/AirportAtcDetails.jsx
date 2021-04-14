6c7fb9731f98485695555e1cc60bbe4bee29ce78
Oren Geva <o4oren@gmail.com>
2021-04-15 02:19:05 +0300

    Update README.md

--- app/components/clientDetails/AirportAtcDetails.jsx
+++ app/components/clientDetails/AirportAtcDetails.jsx
@@ -4,28 +4,28 @@ import {Card} from 'react-native-paper';
 import {StyleSheet, View} from 'react-native';
 import AtcDetails from './AtcDetails';
 
+const getAtcClients =  (airportAtc, airport) => {
+    let atisExists = false;
+    return airportAtc[airport.icao]
+        .sort((a, b) => {
+            if(a.callsign.endsWith('ATIS')) {
+                atisExists = true;
+                return 1;
+            }
+            return b.facility - a.facility;
+        })
+        .map((atc) => {
+            return <AtcDetails
+                atc={atc}
+                showAtis={(!atisExists || atc.callsign.endsWith('ATIS'))}
+                key={atc.key}
+            />;
+        });
+};
+
 export default function AirportAtcDetils(props) {
     const airport = props.airport;
     const airportAtc = useSelector(state => state.vatsimLiveData.clients.airportAtc);
-
-    const getAtcClients =  () => {
-        let atisExists = false;
-        return airportAtc[airport.icao]
-            .sort((a, b) => {
-                if(a.callsign.endsWith('ATIS')) {
-                    atisExists = true;
-                    return 1;
-                }
-                return b.facility - a.facility;
-            })
-            .map((atc) => {
-                return <AtcDetails
-                    atc={atc}
-                    showAtis={(!atisExists || atc.callsign.endsWith('ATIS'))}
-                    key={atc.key}
-                />;
-            });
-    };
 
     if(!airportAtc[airport.icao]) return null;
     return <View style={styles.container}>
@@ -33,7 +33,7 @@ export default function AirportAtcDetils(props) {
             title = {airport.icao}
             subtitle = {airport.name}
         />
-        {getAtcClients()}
+        {getAtcClients(airportAtc, airport)}
     </View>;
 }
 
