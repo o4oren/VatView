c948f9d4a2d06cbd365f20446d6788f7640944f5
Oren Geva <o4oren@gmail.com>
2021-04-14 11:56:22 +0300

    move pure functions out of components

--- app/components/vatsimMapView/MapComponent.jsx
+++ app/components/vatsimMapView/MapComponent.jsx
@@ -13,41 +13,6 @@ const MapComponent = ({clients, selectedClient, airports, screenSize}) => {
     const dispatch = useDispatch();
     const mapRef = useRef(null);
 
-    const renderFromPath = () => {
-        if(selectedClient != null && selectedClient.flight_plan != null && selectedClient.flight_plan.departure != null) {
-            const depAirport = airports.icao[selectedClient.flight_plan.departure];
-            if(depAirport && depAirport.latitude) {
-                return 	<Polyline
-                    coordinates={[
-                        { latitude: depAirport.latitude, longitude: depAirport.longitude },
-                        { latitude: selectedClient.latitude, longitude: selectedClient.longitude }
-                    ]}
-                    strokeColor="red"
-                    geodesic={true}
-                    strokeWidth={3}
-                    key={`${selectedClient.callsign}_path`}
-                />;
-            }
-        }
-    };
-
-    const renderToPath = () => {
-        if(selectedClient != null && selectedClient.flight_plan != null && selectedClient.flight_plan.arrival != null) {
-            const destAirport = airports.icao[selectedClient.flight_plan.arrival];
-            if(destAirport && destAirport.latitude) {
-                return 	<Polyline
-                    coordinates={[
-                        { latitude: selectedClient.latitude, longitude: selectedClient.longitude },
-                        { latitude: destAirport.latitude, longitude: destAirport.longitude }
-                    ]}
-                    strokeColor="green"
-                    geodesic={true}
-                    strokeWidth={3}
-                />;
-            }
-        }
-    };
-
     // if(mapRef && mapRef.current) console.log(mapRef.current.props.children[0].sort((a,b) => {
     //     return (a.key > b.key) ? 1 : ((b.key > a.key) ? -1 : 0);
     // }));
@@ -61,6 +26,7 @@ const MapComponent = ({clients, selectedClient, airports, screenSize}) => {
     //     })
     //     prevMarkers = [];
     // }
+
     return <MapView
         ref={mapRef}
         style={[styles.mapStyle, {width: screenSize.width, height: screenSize.height}]}
@@ -70,19 +36,55 @@ const MapComponent = ({clients, selectedClient, airports, screenSize}) => {
         initialRegion={app.initialRegion}
         onRegionChangeComplete={region => dispatch(allActions.appActions.saveInitialRegion(region))}
     >
-        {
-            [
-                generateCtrPolygons(clients.ctr, clients.fss),
-                generatePilotMarkers(clients.pilots),
-                generateAirportMarkers(clients.airportAtc, airports),
-            ].flat(1)
-        }
-        {/*{generateCtrPolygons(clients.ctr, clients.fss)}*/}
-        {/*{generatePilotMarkers(clients.pilots)}*/}
-        {/*{generateAirportMarkers(clients.airportAtc, airports)}*/}
-        {renderFromPath()}
-        {renderToPath()}
+        {getMarkers(clients, airports)}
+        {renderFromPath(airports, selectedClient)}
+        {renderToPath(airports, selectedClient)}
     </MapView>;
+};
+
+const getMarkers = (clients, airports) => {
+    const markers = [
+        generateCtrPolygons(clients.ctr, clients.fss),
+        generatePilotMarkers(clients.pilots),
+        generateAirportMarkers(clients.airportAtc, airports),
+    ].flat(1);
+    console.log('markers', markers);
+    return markers;
+};
+
+const renderFromPath = (airports, selectedClient) => {
+    if(selectedClient != null && selectedClient.flight_plan != null && selectedClient.flight_plan.departure != null) {
+        const depAirport = airports.icao[selectedClient.flight_plan.departure];
+        if(depAirport && depAirport.latitude) {
+            return 	<Polyline
+                coordinates={[
+                    { latitude: depAirport.latitude, longitude: depAirport.longitude },
+                    { latitude: selectedClient.latitude, longitude: selectedClient.longitude }
+                ]}
+                strokeColor="red"
+                geodesic={true}
+                strokeWidth={3}
+                key={`${selectedClient.callsign}_path`}
+            />;
+        }
+    }
+};
+
+const renderToPath = (airports, selectedClient) => {
+    if(selectedClient != null && selectedClient.flight_plan != null && selectedClient.flight_plan.arrival != null) {
+        const destAirport = airports.icao[selectedClient.flight_plan.arrival];
+        if(destAirport && destAirport.latitude) {
+            return 	<Polyline
+                coordinates={[
+                    { latitude: selectedClient.latitude, longitude: selectedClient.longitude },
+                    { latitude: destAirport.latitude, longitude: destAirport.longitude }
+                ]}
+                strokeColor="green"
+                geodesic={true}
+                strokeWidth={3}
+            />;
+        }
+    }
 };
 
 const styles = StyleSheet.create({
