ba56fc769472b85056c83385892160600b38c60e
Oren Geva <o4oren@gmail.com>
2021-05-18 18:17:08 +0300

    polyfills for paper-dates

--- App.js
+++ App.js
@@ -13,6 +13,54 @@ import AppLoading from 'expo-app-loading';
 
 const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
 
+// on top of your index.android.js file
+const isAndroid = require('react-native').Platform.OS === 'android';
+const isHermesEnabled = !!global.HermesInternal;
+
+// polyfills for Intl - required by dates
+if (isHermesEnabled || isAndroid) {
+
+    require('@formatjs/intl-getcanonicallocales/polyfill');
+    require('@formatjs/intl-locale/polyfill');
+
+
+    require('@formatjs/intl-pluralrules/polyfill');
+    require('@formatjs/intl-pluralrules/locale-data/en.js'); // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+
+    require('@formatjs/intl-displaynames/polyfill');
+    require('@formatjs/intl-displaynames/locale-data/en.js'); // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+
+    require('@formatjs/intl-listformat/polyfill');
+    require('@formatjs/intl-listformat/locale-data/en.js'); // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+
+    require('@formatjs/intl-numberformat/polyfill');
+    require('@formatjs/intl-numberformat/locale-data/en.js'); // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+
+    require('@formatjs/intl-relativetimeformat/polyfill');
+    require('@formatjs/intl-relativetimeformat/locale-data/en.js'); // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+
+    require('@formatjs/intl-datetimeformat/polyfill');
+    require('@formatjs/intl-datetimeformat/locale-data/en.js'); // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+
+    require('@formatjs/intl-datetimeformat/add-golden-tz.js');
+
+
+
+    // https://formatjs.io/docs/polyfills/intl-datetimeformat/#default-timezone
+
+    if ('__setDefaultTimeZone' in Intl.DateTimeFormat) {
+
+        // If you are using react-native-cli
+        // let RNLocalize = require('react-native-localize');
+        // Intl.DateTimeFormat.__setDefaultTimeZone(RNLocalize.getTimeZone());
+
+        //  Are you using Expo, use this instead of previous 2 lines
+        Intl.DateTimeFormat.__setDefaultTimeZone(
+            require('expo-localization').timezone
+        );
+    }
+}
+
 export default function App() {
     const [state, setState] = useState({isReady: false});
 
