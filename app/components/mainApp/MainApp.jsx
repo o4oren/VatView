7433fb75d861f647804b9bb92b9c46b5aaaf2e65
Oren Geva <o4oren@gmail.com>
2021-05-23 23:47:30 +0300

    Update every 15 seconds - not show all aircraft at the same time

--- app/components/mainApp/MainApp.jsx
+++ app/components/mainApp/MainApp.jsx
@@ -49,7 +49,7 @@ export default function mainApp() {
         if(staticAirspaceData.firBoundaries != null && Object.keys(staticAirspaceData.firs).length) {
             console.log('starting to get data feed');
             dispatch(allActions.vatsimLiveDataActions.updateData);
-            const interval = setInterval(() => dispatch(allActions.vatsimLiveDataActions.updateData), 60 * 1000);
+            const interval = setInterval(() => dispatch(allActions.vatsimLiveDataActions.updateData), 15 * 1000);
             return () => {
                 clearInterval(interval);
             };
