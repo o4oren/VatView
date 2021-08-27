e1af2ffa4b8dfe93d100c80013554e1eb6373264
Oren Geva <o4oren@gmail.com>
2021-08-27 18:47:56 +0300

    completed weather parsing

--- app/components/MetarView/MetarView.jsx
+++ app/components/MetarView/MetarView.jsx
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
-import {Searchbar, Text} from 'react-native-paper';
+import {Divider, Searchbar, Text} from 'react-native-paper';
 import allActions from '../../redux/actions';
+import {getAirportByCode} from '../../common/airportTools';
 
 export default function MetarView(props) {
     const metar = useSelector(state => state.metar.metar);
+    const airports = useSelector(state => state.staticAirspaceData.airports);
     const [searchTerm, setSearchTerm] = useState('');
     const dispatch = useDispatch();
 
@@ -14,6 +16,43 @@ export default function MetarView(props) {
         if(searchTerm.length === 4)
             dispatch(allActions.metarActions.metarRequsted(searchTerm));
     };
+
+    function displayClouds() {
+        if(!metar.clouds || metar.clouds.length === 0) {
+            return <Text>No clouds</Text>;
+        }
+        return <View>
+            <Text>Clouds:</Text>
+            {metar.clouds.map(layer => {
+                return <Text>{layer.code} at {layer.base_feet_agl} ft AGL</Text>;
+            })}
+        </View>;
+    }
+
+    function displayMetar() {
+        console.log(metar);
+        if(metar && Object.keys(metar).length > 0) {
+            return   <View style={styles.metarDisplay}>
+                <Text>{metar.raw_text}</Text>
+                <Divider style={styles.divider}/>
+                <Text>{airports && airports.icao && getAirportByCode(metar.icao, airports) ? getAirportByCode(metar.icao, airports).name : ''}</Text>
+                <Text>Observed at {metar.observed.toUTCString()}</Text>
+                <Text>Pressure: {Number(metar.barometer.hg).toFixed(2)} hg / {metar.barometer.mb} mb</Text>
+                <Text>Temperature: {metar.temperature.celsius} &#x2103; / {Number(metar.temperature.fahrenheit).toFixed(0)} &#x2109;</Text>
+                <Text>Due Point: {metar.dewpoint.celsius}c / {Number(metar.dewpoint.fahrenheit).toFixed(0)}f</Text>
+                <Text>Winds: {metar.wind.degrees} at {Number(metar.wind.speed_kts).toFixed(0)} kts {}</Text>
+                {metar.wind.speed_kts != metar.wind.gust_kts ?
+                    <Text>Gust: {Number(metar.wind.gust_kts).toFixed(0)} kts {}</Text>
+                    : null}
+                <Text>Visibility: {metar.visibility.miles} sm</Text>
+                {metar.ceiling ? <Text>Ceiling: {metar.ceiling.code} @ {metar.ceiling.feet_agl} ft AGL</Text> : <View />}
+                {displayClouds()}
+                <Text>Humidity: {Number(metar.humidity_percent).toFixed(0)}%</Text>
+                <Text>Flight conditions: {metar.flight_category}</Text>
+            </View>;
+        }
+        return null;
+    }
 
     return <View>
         <View style={styles.container}>
@@ -25,7 +64,8 @@ export default function MetarView(props) {
                 value={searchTerm}
             />
         </View>
-        <Text>{metar}</Text>
+        {displayMetar()}
+
     </View>;
 }
 
@@ -35,11 +75,18 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'row',
-        padding: 5
+        padding: 15,
+    },
+    metarDisplay: {
+        padding: 15
     },
     textInput: {
         flex: 1,
         borderRadius: 25,
         maxWidth: 300
+    },
+    divider: {
+        marginTop: 5,
+        marginBottom: 5
     }
 });
--- app/redux/actions/metarActions.js
+++ app/redux/actions/metarActions.js
@@ -1,4 +1,5 @@
 import {DATA_FETCH_ERROR} from './vatsimLiveDataActions';
+import metarParser from 'aewx-metar-parser';
 export const METAR_REQUESTED = 'METAR_REQUESTED';
 export const METAR_UPDATED = 'METAR_UPDATED';
 
@@ -10,8 +11,10 @@ const metarRequsted = (icao) => {
                 'https://metar.vatsim.net/data/metar.php?id=' + icao
             );
             let metar = await response.text();
-            console.log(metar);
-            dispatch(metarUpdated(metar));
+            const metarObject = metarParser(metar);
+            console.log(metarObject);
+
+            dispatch(metarUpdated(metarObject));
         } catch (error) {
             console.log(error);
             dispatch({type: DATA_FETCH_ERROR});
