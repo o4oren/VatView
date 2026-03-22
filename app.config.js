14341c34d81935cedc0da5e3ea1da3b3a60e2147
Oren Geva <oren_geva@intuit.com>
2026-03-22 12:54:21 +0200

    Bump version to 2.0.1

--- app.config.js
+++ app.config.js
@@ -28,7 +28,7 @@ module.exports = {
         name: 'VatView',
         slug: 'vatview',
         platforms: ['ios', 'android'],
-        version: '2.0.0',
+        version: '2.0.1',
         orientation: 'default',
         userInterfaceStyle: 'automatic',
         icon: './graphics/icons/icon-1024-background.png',
